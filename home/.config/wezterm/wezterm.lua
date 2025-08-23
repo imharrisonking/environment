72bdb58cd432fbea84211e1939b4c376c1ae5e54
Harrison King <hi@harrisonk.ing>
2025-08-23 08:55:49 +0000

    Initial commit

---
+++ home/.config/wezterm/wezterm.lua
@@ -0,0 +1,12 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+
+config.color_scheme = 'tokyonight_night'
+config.font = wezterm.font 'TX-02'
+config.font_size = 14
+config.enable_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.enable_wayland = false
+
+return config
