d2feac91e8443dba173aec26ada654e602a6a87b
imharrisonking <harrison.k@hotmail.co.uk>
2025-09-02 19:30:21 +0100

    tpipeline updates

--- home/.config/nvim/lua/custom/plugins/tpipeline.lua
+++ home/.config/nvim/lua/custom/plugins/tpipeline.lua
@@ -9,6 +9,16 @@ return {
     vim.g.tpipeline_autoembed = 0
     vim.g.tpipeline_restore = 1
     vim.g.tpipeline_clearstl = 1
+    
+    -- Force statusline to stay hidden in all modes including command mode
+    vim.g.tpipeline_preservebg = 1
+    
+    -- Ensure statusline is always hidden
+    vim.api.nvim_create_autocmd({ "ModeChanged", "CmdlineEnter", "CmdlineLeave" }, {
+      callback = function()
+        vim.opt.laststatus = 0
+      end,
+    })
 
     -- Let tpipeline automatically use your existing lualine statusline
     -- No need to redefine - it should detect and use lualine automatically
